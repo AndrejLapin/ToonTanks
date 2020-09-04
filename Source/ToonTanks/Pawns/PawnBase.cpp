6aff8b5b2e090bac3ee3b14d0d7979d32ef2929c
Andrej Lapin <lapin.andrej1@gmail.com>
2020-09-04 10:36:10 +0300

    added Pawn Tank and Pawn turret base classes, impelmented movement functions and camera for PawnTank class, Implementet turret rotations for Base Pawn class

---
+++ Source/ToonTanks/Pawns/PawnBase.cpp
@@ -0,0 +1,45 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PawnBase.h"
+#include "Components/CapsuleComponent.h"
+
+// Sets default values
+APawnBase::APawnBase()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	CapsuleComp = CreateDefaultSubobject<UCapsuleComponent>(TEXT("Capsule Collider"));
+	RootComponent = CapsuleComp;
+
+	BaseMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Base Mesh"));
+	BaseMesh->SetupAttachment(RootComponent);
+
+	TurretMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Turret Mesh"));
+	TurretMesh->SetupAttachment(BaseMesh);
+
+	ProjectileSpawnPoint = CreateDefaultSubobject<USceneComponent>(TEXT("Projectile Spawn Point"));
+	ProjectileSpawnPoint->SetupAttachment(TurretMesh);
+
+}
+
+void APawnBase::RotateTurretFunction(FVector LooAtTarget)
+{
+	FVector LookAtTargetClean = FVector(LooAtTarget.X, LooAtTarget.Y, TurretMesh->GetComponentLocation().Z);
+	FVector StartLocation = TurretMesh->GetComponentLocation();
+
+	FRotator TurretRotation = FVector(LookAtTargetClean - StartLocation).Rotation();
+	TurretMesh->SetWorldRotation(TurretRotation);
+}
+
+void APawnBase::Fire()
+{
+	UE_LOG(LogTemp, Warning, TEXT("Fire Condition Success"));
+}
+
+void APawnBase::HandleDestruction()
+{
+
+}
+
